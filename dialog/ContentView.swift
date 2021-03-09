df88105df7d5c2392ebc45a4b9e0f85fa0c31b2d
Bart Reardon <bart.reardon@csiro.au>
2021-03-09 19:39:57 +1100

    Initial Commit

---
+++ dialog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dialog
+//
+//  Created by Bart Reardon on 9/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dialog/dialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  dialogApp.swift
+//  dialog
+//
+//  Created by Bart Reardon on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct dialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
