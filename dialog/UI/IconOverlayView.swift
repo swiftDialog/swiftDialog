84624120fd264f4d1ba4f21d88a8574d9731adcb
Bart Reardon <bart.reardon@csiro.au>
2021-03-21 00:02:32 +1100

    Updated built-in icon processing (need to remove hard coding) added white background fill so transparent parts of the warning and caution icons weren't transparent Updated method of calling built in icons. Can now be called using `--icon caution` or `--icon warning` or `--icon info` (in addition to he existing documented methods Can now specify the overlay icon to be one of the built in types with `--overlayicon warning` etc.

--- dialog/UI/IconOverlayView.swift
+++ dialog/UI/IconOverlayView.swift
@@ -16,6 +16,11 @@ struct IconOverlayView: View {
     var imgFromURL: Bool = false
     var imgFromAPP: Bool = false
     
+    var builtInIconName: String = ""
+    var builtInIconFill: String = ""
+    var builtInIconColour: Color = Color.black
+    var builtInIconPresent: Bool = false
+    
     init(overlayWidth: CGFloat? = nil, overlayHeight: CGFloat? = nil) {
         self.overlayWidth = appvars.imageWidth/2
         self.overlayHeight = appvars.imageHeight/2
@@ -25,42 +30,79 @@ struct IconOverlayView: View {
         if overlayImagePath.hasSuffix(".app") {
             imgFromAPP = true
         }
+        
+        if overlayImagePath == "warning" {
+            builtInIconName = "exclamationmark.octagon.fill"
+            builtInIconFill = "octagon.fill"
+            builtInIconColour = Color.red
+            builtInIconPresent = true
+        }
+        if overlayImagePath == "caution" {
+            builtInIconName = "exclamationmark.triangle.fill"
+            builtInIconFill = "triangle.fill"
+            builtInIconColour = Color.yellow
+            builtInIconPresent = true
+        }
+        if overlayImagePath == "info" {
+            builtInIconName = "person.fill.questionmark"
+            builtInIconPresent = true
+        }
     }
     
     var body: some View {
         if CLOptionPresent(OptionName: CLOptions.overlayIconOption) {
             ZStack {
-                if imgFromURL {
-                    let webImage: NSImage = getImageFromPath(fileImagePath: overlayImagePath, imgWidth: appvars.imageWidth, imgHeight: appvars.imageHeight)
-                    //let webImage: NSImage = Utils().getImageFromHTTPURL(fileURLString: overlayImagePath)
-                    Image(nsImage: webImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .scaledToFit()
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                } else if imgFromAPP {
-                    Image(nsImage: getAppIcon(appPath: overlayImagePath, withSize: overlayWidth!))
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .scaledToFit()
-                } else if FileManager.default.fileExists(atPath: overlayImagePath) {
-                    let diskImage: NSImage = getImageFromPath(fileImagePath: overlayImagePath, imgWidth: appvars.imageWidth, imgHeight: appvars.imageHeight)
-                    Image(nsImage: diskImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .scaledToFit()
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                } else {
-                    Image(systemName: "questionmark.square.dashed")
+                if builtInIconPresent {
+                    // background colour
+                    ZStack {
+                        Image(systemName: builtInIconFill)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .scaledToFit()
-                            .foregroundColor(Color.black)
+                            .foregroundColor(Color.white)
+                    }
+                    //forground image
+                    ZStack {
+                        Image(systemName: builtInIconName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaledToFit()
+                            .foregroundColor(builtInIconColour)
+                    }
+                } else {
+                    if imgFromURL {
+                        let webImage: NSImage = getImageFromPath(fileImagePath: overlayImagePath, imgWidth: appvars.imageWidth, imgHeight: appvars.imageHeight)
+                        //let webImage: NSImage = Utils().getImageFromHTTPURL(fileURLString: overlayImagePath)
+                        Image(nsImage: webImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .shadow(radius: 5)
+                    } else if imgFromAPP {
+                        Image(nsImage: getAppIcon(appPath: overlayImagePath, withSize: overlayWidth!))
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaledToFit()
+                    } else if FileManager.default.fileExists(atPath: overlayImagePath) {
+                        let diskImage: NSImage = getImageFromPath(fileImagePath: overlayImagePath, imgWidth: appvars.imageWidth, imgHeight: appvars.imageHeight)
+                        Image(nsImage: diskImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .shadow(radius: 5)
+                    } else {
+                        Image(systemName: "questionmark.square.dashed")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .scaledToFit()
+                                .foregroundColor(Color.black)
+                    }
                 }
             }
             .frame(width: overlayWidth, height: overlayHeight)
             .offset(x: 40, y: 50)
-            .shadow(radius: 5)
         }
     }
 }
