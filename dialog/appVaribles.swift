284e3657e87354955f2a8cab0b0bd69ccf85b8d6
Bart Reardon <bart.reardon@csiro.au>
2021-08-15 19:56:57 +1000

    Added in all the help text

--- dialog/appVaribles.swift
+++ dialog/appVaribles.swift
@@ -30,6 +30,13 @@ var helpText = """
                     Markdown follows the CommonMark Spec https://spec.commonmark.org/current/
                     The message can be of any length. If it is larger than the viewable area
                     The message contents will be presented in  scrolable area.
+    
+        -\(CLOptions.mainImage.short), --\(CLOptions.mainImage.long)  <file> | <url>
+                    Display an image instead of a message.
+                    Images will be resized to fit the available display area
+    
+                    --\(CLOptions.mainImageCaption.long) <text>
+                        Text that will appear underneath the displayed image.
         
         -\(CLOptions.iconOption.short), --\(CLOptions.iconOption.long) <file> | <url>
                     Set the icon to display
@@ -185,8 +192,22 @@ var helpText = """
                             bold (default)
     
                     Example: \"colour=#00A4C7,weight=light,size=60\"
-
-
+    
+        --\(CLOptions.windowWidth.long) <number>
+                    Sets the width of the dialog window to the specified width in points
+    
+        --\(CLOptions.windowHeight.long) <number>
+                    Sets the height of the dialog window to the specified height in points
+    
+        --\(CLOptions.timerBar.long) (<seconds>)
+                    Replaces default button with a timer countdown after which dialog will close with exit code 4
+                    Default timer value is 10 seconds
+                    Optional value <seconds> can be specified to the desired value
+    
+                    If used in conjuction with --\(CLOptions.button1TextOption.long) the default button
+                    will be displayed but will be disabled for the first 3 seconds of the timer, after which it
+                    becomes active and can be used to dismiss dialog with the standard button 1 exit code of 0
+    
         -\(CLOptions.lockWindow.short), --\(CLOptions.lockWindow.long)
                     Let window me moved around the screen. Default is not moveable
 
@@ -201,10 +222,11 @@ var helpText = """
     
         -\(CLOptions.jsonOutPut.short), --\(CLOptions.jsonOutPut.long)
                     Outputs any results in json format for easier processing
-                    (currently limited to dropdown item selections)
+                    (for dropdown item selections and textfield responses)
 
         -\(CLOptions.ignoreDND.short), --\(CLOptions.ignoreDND.long)
                     Will ignore user Do Not Disturb setting
+                        (only works in macOS 11)
     
         -\(CLOptions.getVersion.short), --\(CLOptions.getVersion.long)
                     Prints the app version
