b4734ee0e0929595e0c1394225557757f5dbd7f2
Bart Reardon <bart.reardon@csiro.au>
2022-03-13 15:51:09 +1100

    tidy up and animations

--- dialog/UI/TextEntryView.swift
+++ dialog/UI/TextEntryView.swift
@@ -13,17 +13,22 @@ struct TextEntryView: View {
     
     @State var textFieldValue = Array(repeating: "", count: textFields.count)
     
+    @State private var animationAmount = 1.0
+    
     var textFieldPresent: Bool = false
     var fieldwidth: CGFloat = 0
-    var highlight = [Color]()
+    var requiredFieldsPresent : Bool = false
     
     init(observedDialogContent : DialogUpdatableContent) {
         self.observedDialogContent = observedDialogContent
         if cloptions.textField.present {
             textFieldPresent = true
-            for _ in 0..<textFields.count {
+            for i in 0..<textFields.count {
                 textFieldValue.append(" ")
-                highlight.append(Color.clear)
+                if textFields[i].required {
+                    requiredFieldsPresent = true
+                }
+                //highlight.append(Color.clear)
             }
         }
         if cloptions.hideIcon.present {
@@ -39,7 +44,7 @@ struct TextEntryView: View {
                 ForEach(0..<textFields.count, id: \.self) {index in
                     HStack {
                         Spacer()
-                        Text(textFields[index].title)
+                        Text(textFields[index].title + (textFields[index].required ? " *":""))
                             .bold()
                             .font(.system(size: 15))
                             .frame(idealWidth: fieldwidth*0.20, maxWidth: 150, alignment: .leading)
@@ -51,19 +56,12 @@ struct TextEntryView: View {
                                     SecureField("", text: $textFieldValue[index])
                                         .disableAutocorrection(true)
                                         .textContentType(.password)
-                                        //.border(highlight[index])
-                                        .overlay(RoundedRectangle(cornerRadius: 5)
-                                                    .stroke(observedDialogContent.requiredTextfieldHighlight[index], lineWidth: 1))
-
                                     Image(systemName: "lock.fill")
                                         .foregroundColor(stringToColour("#008815")).opacity(0.5)
                                             .frame(idealWidth: fieldwidth*0.50, maxWidth: 300, alignment: .trailing)
                                 }
                             } else {
                                 TextField("", text: $textFieldValue[index])
-                                    //.border(highlight[index])
-                                    .overlay(RoundedRectangle(cornerRadius: 5)
-                                                .stroke(observedDialogContent.requiredTextfieldHighlight[index], lineWidth: 1))
                             }
                         }
                         .frame(idealWidth: fieldwidth*0.50, maxWidth: 300, alignment: .trailing)
@@ -71,7 +69,22 @@ struct TextEntryView: View {
                             //update appvars with the text that was entered. this will be printed to stdout on exit
                             textFields[index].value = textFieldValue[index]
                         })
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                                    .stroke(observedDialogContent.requiredTextfieldHighlight[index], lineWidth: 1)
+                                    .animation(.easeIn(duration: 0.2)
+                                                .repeatCount(3, autoreverses: true)
+                                               )
+                                 )
                         Spacer()
+                    }
+                }
+                if requiredFieldsPresent {
+                    HStack {
+                        Spacer()
+                        Text("* Required Fields")
+                            .font(.system(size: 10)
+                                    .weight(.light))
+                            .padding(.trailing, 10)
                     }
                 }
             }
