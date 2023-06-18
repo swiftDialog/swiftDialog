fdcb6f021f835948f93b7a34605210d5f5f8cbd8
Bart Reardon <bart.reardon@me.com>
2023-06-18 20:51:16 +1000

    More markdown support

--- dialog/extras/Extensions.swift
+++ dialog/extras/Extensions.swift
@@ -208,6 +208,9 @@ extension Theme {
     .link {
         ForegroundColor(.link)
     }
+    .strong {
+          FontWeight(.semibold)
+        }
     .codeBlock { configuration in
         ScrollView(.horizontal) {
             configuration.label
@@ -218,9 +221,92 @@ extension Theme {
                 }
                 .padding(16)
         }
-        .background(Color.secondaryBackground)
+        .background(.background)
         .clipShape(RoundedRectangle(cornerRadius: 6))
-        .markdownMargin(top: 0, bottom: 16)
+        .markdownMargin(top: 10, bottom: 10)
+    }
+    .heading1 { configuration in
+      VStack(alignment: .leading, spacing: 0) {
+        configuration.label
+          .relativePadding(.bottom, length: .em(0.3))
+          .relativeLineSpacing(.em(0.125))
+          .markdownMargin(top: 24, bottom: 16)
+          .markdownTextStyle {
+            FontWeight(.semibold)
+            FontSize(.em(2))
+          }
+        Divider().overlay(Color.divider)
+      }
+    }
+    .heading2 { configuration in
+      VStack(alignment: .leading, spacing: 0) {
+        configuration.label
+          .relativePadding(.bottom, length: .em(0.3))
+          .relativeLineSpacing(.em(0.125))
+          .markdownMargin(top: 24, bottom: 16)
+          .markdownTextStyle {
+            FontWeight(.semibold)
+            FontSize(.em(1.5))
+          }
+        Divider().overlay(Color.divider)
+      }
+    }
+    .heading3 { configuration in
+      configuration.label
+        .relativeLineSpacing(.em(0.125))
+        .markdownMargin(top: 24, bottom: 16)
+        .markdownTextStyle {
+          FontWeight(.semibold)
+          FontSize(.em(1.25))
+        }
+    }
+    .heading4 { configuration in
+      configuration.label
+        .relativeLineSpacing(.em(0.125))
+        .markdownMargin(top: 24, bottom: 16)
+        .markdownTextStyle {
+          FontWeight(.semibold)
+        }
+    }
+    .heading5 { configuration in
+      configuration.label
+        .relativeLineSpacing(.em(0.125))
+        .markdownMargin(top: 24, bottom: 16)
+        .markdownTextStyle {
+          FontWeight(.semibold)
+          FontSize(.em(0.875))
+        }
+    }
+    .heading6 { configuration in
+      configuration.label
+        .relativeLineSpacing(.em(0.125))
+        .markdownMargin(top: 24, bottom: 16)
+        .markdownTextStyle {
+          FontWeight(.semibold)
+          FontSize(.em(0.85))
+          ForegroundColor(.tertiaryText)
+        }
+    }
+    .table { configuration in
+      configuration.label
+        .fixedSize(horizontal: false, vertical: true)
+        .markdownTableBorderStyle(.init(color: .border))
+        .markdownMargin(top: 16, bottom: 16)
+        .markdownTableBackgroundStyle(
+          .alternatingRows(Color.clear, Color.clear, header: Color.background)
+        )
+    }
+    .tableCell { configuration in
+      configuration.label
+        .markdownTextStyle {
+          if configuration.row == 0 {
+            FontWeight(.semibold)
+          }
+        }
+        .fixedSize(horizontal: false, vertical: true)
+        .padding(.vertical, 6)
+        .padding(.horizontal, 13)
+        .relativeLineSpacing(.em(0.25))
     }
 }
 
