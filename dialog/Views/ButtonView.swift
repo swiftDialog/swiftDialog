f5a3371dec48e7bd5cdbef8d2b09dd066c65cfe9
AMJones <am@jonesiscoding.com>
2024-06-21 16:40:37 -0500

    Adds timer to dialog when using buttonstyle 'stack'.

--- dialog/Views/ButtonView.swift
+++ dialog/Views/ButtonView.swift
@@ -99,6 +99,10 @@ struct ButtonView: View {
                     )
                     .keyboardShortcut(observedData.appProperties.button2DefaultAction)
                 }
+                if observedData.args.timerBar.present {
+                    TimerView(progressSteps: progressSteps, visible: !observedData.args.hideTimerBar.present, observedDialogContent: observedData)
+                        .frame(alignment: .bottom )
+                }
             }
         } else {
             // Buttons
